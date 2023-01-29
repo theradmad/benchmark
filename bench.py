5bb23846c652ad2de5da78fec6479eb66811bdba
theradmad <madeline.bumpus@bison.howard.edu>
2023-01-29 16:19:56 -0500

    2

--- bench.py
+++ bench.py
@@ -6,4 +6,11 @@ def benchmark1():
     start_time = time.time()
 
     for i in range(10**10): 
+        6+7
+    for i in range(5*10**9): 
+        6*7
+    for i in range(2*10**9): 
+        6/7
+
+
     
